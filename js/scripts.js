4a0ea315de620ee668724b3dffc566ea71025db2
jaybojaybojaybo <jasun.feddema@gmail.com>
2017-04-01 14:38:28 -0700

    allow the creation of new Pizza objects by the user and create an area in the HTML to display the results.

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,45 @@
-// prototype DoMath
-//
-// function Pizza
+var premiumToppings = [];
+var toppings = [];
 
+function Pizza(size,sauce,extra,premiums,tops) {
+  this.size = size;
+  this.sauce = sauce;
+  this.extra = extra;
+  this.premiums = premiumToppings;
+  this.tops = toppings;
+}
 
-
-$(function () {
-  var premiumToppings = [];
-  var toppings = [];
+$(document).ready(function () {
+  var size;
+  var sauce;
+  var extra;
 
   $("form#pizzaOptions").submit(function(event){
     event.preventDefault();
-
-    var size = $('input[name="size"]:checked').val();
-    var sauce = $('input[name="sauce"]:checked').val();
-    var extra = $('input[name="extra"]:checked').val();
-    $('input[name="premium"]:checked').each(function() {
+    size = $('input[name="size"]:checked').val();
+    sauce = $('input[name="sauce"]:checked').val();
+    extra = $('input[name="extra"]:checked').val();
+    $("input:checkbox[name=premium]:checked").each(function() {
       var premiumTop = $(this).val();
       premiumToppings.push(premiumTop);
+      return premiumToppings;
     });
-    $('input[name="topping"]:checked').each(function() {
+    $('input:checkbox[name=topping]:checked').each(function() {
       var regulartop = $(this).val();
       toppings.push(regulartop);
+      return toppings;
+    });
+
+    var yourPizza = new Pizza(size,sauce,extra,premiumToppings,toppings);
+    $("#confirmation").show();
+    $("#pizzaSize").text(yourPizza.size);
+    $("#pizzaExtraSauce").text(yourPizza.extra + " ");
+    $("#pizzaSauce").text(yourPizza.sauce);
+    premiumToppings.forEach(function(topping) {
+      $("ul#pizzaToppings").append("<li>" + topping + "</li>");
+    });
+    toppings.forEach(function(topping) {
+      $("ul#pizzaToppings").append("<li>" + topping + "</li>");
     });
 
 console.log(size);
@@ -28,5 +47,8 @@ console.log(sauce);
 console.log(extra);
 console.log(premiumToppings);
 console.log(toppings);
+
+
+
   });
 });
